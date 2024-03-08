764319e71836a60dea919092d5275b8a8839607a
Maksim.Surimov <MaxxLorge@gmail.com>
2024-03-08 17:57:50 +0500

    add data annotations

--- IssuerPersonalAccount/ViewModels/SignInViewModel.cs
+++ IssuerPersonalAccount/ViewModels/SignInViewModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IssuerPersonalAccount.ViewModels;
 
 public class SignInViewModel
 {
+    [Required(ErrorMessage = "Небходимо ввести логин", AllowEmptyStrings = false)]
     public string Login { get; set; }
+    
+    [Required(ErrorMessage = "Необходимо ввести пароль", AllowEmptyStrings = false)]
     public string Password { get; set; }
 }
